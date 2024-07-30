b1f686c2f5a70bb510b202a45e3041aad9c7be6d
alassane8 <alassane.wade@edu.ece.fr>
2024-07-30 18:40:04 +0200

    feat: add comments to code + delete path

--- code/script.py
+++ code/script.py
@@ -2,36 +2,40 @@ import re
 from pathlib import Path
 
 ########################################################################################################################
-def tree(file, project, s):
+
+def tree(file, project, space):
     if project.isRoot():
         if project.framework == '':
-            file.write("\t\t" * s + project.name + ": Could not retrieve framework\n")
+            file.write("\t\t" * space + project.name + ": Could not retrieve framework\n")
         else:
-            file.write("\t\t" * s + project.name + ": " + project.framework + "\n")
+            file.write("\t\t" * space + project.name + ": " + project.framework + "\n")
     else:
-        for i in range(s):
+        for i in range(space):
             file.write("\t\t" * 2 + "|") * i
         if project.framework == '':
             file.write("------- " + project.name + ": Could not retrieve framework\n")
         else:
             file.write("------- " + project.name + ": " + project.framework + "\n")
     for child in project.child:
-        tree(file, child, s + 1)
+        tree(file, child, space + 1)
 
-#######################################################################################################################
-def open_path(file_to_sln, file_to_csproj, project):
+########################################################################################################################
+
+def open_path(file_to_csproj, project):
     with open(file_to_csproj, 'r') as file:
         file_content = file.read()
+
+        # Looking for missed ref in .csproj that are not in .sln
         path_to_missed_ref = re.findall(pattern=r'<ProjectReference Include="(.*?)".*?>',
                                         string=file_content,
                                         flags=re.M | re.S)
         for references_path in path_to_missed_ref:
             p1 = Path(references_path)
-            print(f"Project Reference: {references_path}")
             project_found = next((p for p in all_projects.values() if p.name == p1.stem), None)
             if project_found is None:
-                print("!!!!!!!!!!!!! Attention, nous n'avons pas le project !!!!!!!!!!!!!")
                 continue
+
+            # Add missed project to dependences
             project.add_mother(project_found)
             project_found.add_child(project)
 
@@ -46,9 +50,13 @@ class Project:
         self.mother: list[Project] = []
         self.child: list[Project] = []
 
+    def add_child(self, child):
+        if any(c for c in self.child if c.name == child.name):
+            return
+        self.child.append(child)
+
     def add_mother(self, mother):
         if any(m for m in self.mother if m.name == mother.name):
-            print(f"[{self.name}] We are trying to add twice the same mother: {mother.name}")
             return
         self.mother.append(mother)
 
@@ -64,100 +72,125 @@ class Project:
         if len(self.child) == 0 and len(self.mother) == 0:
             return True
 
-    def add_child(self, child):
-        if any(c for c in self.child if c.name == child.name):
-            print(f"[{self.name}] We are trying to add twice the same child: {child.name}")
-            return
-        self.child.append(child)
-
     def __str__(self):
         return f"======== ({self.guid}) {self.name} - {self.path} - {self.framework}"
 
 ########################################################################################################################
 
 all_projects: dict[str, Project] = {}
-fileSLN = "C:\Workspace\SigarePlus\SIG-3884_f005094_Harmonisation\SigareCSharp\SigareCSharp.sln"
 
+while True:
+    # User input path to .sln file
+    fileSLN = input("\nEnter path to .sln file:\n")
+
+    if (fileSLN.endswith('.sln')):
+        break
+
+# Path has been checked
 if fileSLN.endswith('.sln'):
     try:
         with open(fileSLN, 'r') as file:
             file_content = file.read()
+
+            # Get project's informations => 1.name 2.path in the solution 3.GUID
             regex_projects = re.findall(pattern=r'Project.*? = "(.*?)", "(.*?)", "({.*?})',
                                         string=file_content,
                                         flags=re.RegexFlag.M)
+
+            # Isolate path to solution
             regex_path = re.findall(pattern=r', ["](.*?)["],',
                                     string=file_content,
                                     flags=re.RegexFlag.M | re.RegexFlag.S)
+
+            # Some projects in .sln are just folders => we don't count them as real projects
             for project_name, project_path, guid in regex_projects:
                 if project_name == project_path:
                     continue
+
+                # else we add project and it's info in dictionnary
                 all_projects[guid] = Project(name=project_name,
                                              guid=guid,
                                              path=project_path,
                                              framework='')
+
+            # Looking for projects that have dependences
             dep_projects = re.findall(pattern=r'Project.*? = "(.*?)", (.*?), (.{40})\n\tProjectSection',
                                       string=file_content,
                                       flags=re.RegexFlag.M)
-            for i in dep_projects:
-                print(i)
+
+            # Links projects GUID and it's dependences GUID
             regex_ProjectSections = re.findall(
                 pattern=r', "(.{38})"\n\tProjectSection.*?\n\t\t(.*?)\n\tEndProjectSection$',
                 string=file_content,
                 flags=re.RegexFlag.M | re.RegexFlag.S)
 
-            for i in range(len(regex_ProjectSections)):
-                guid, childs = regex_ProjectSections[i]
+
+            for project_dependences in range(len(regex_ProjectSections)):
+                guid, childs = regex_ProjectSections[project_dependences]
+
+                # Isolate dependeces GUID
                 regex_Dependences = re.findall(pattern=r" = ({.*?})",
                                                string=childs,
-                                               flags=re.RegexFlag.M | re.RegexFlag.S)
+                                              flags=re.RegexFlag.M | re.RegexFlag.S)
                 if guid not in all_projects:
                     continue
+
+                # Add project and dependences to the dictionnary
                 for mother_guid in regex_Dependences:
                     all_projects[guid].add_mother(all_projects[mother_guid])
                     all_projects[mother_guid].add_child(all_projects[guid])
-    except FileNotFoundError:
-        print("\nFileNotFoundError")
 
-########################################################################################################################
-for guid, project in all_projects.items():
-    file_to_sln = "C:\Workspace\SigarePlus\SIG-3884_f005094_Harmonisation\SigareCSharp\\"
-    file_to_csproj = f"C:\Workspace\SigarePlus\SIG-3884_f005094_Harmonisation\SigareCSharp\{project.path}"
-    try:
-        with open(file_to_csproj, 'r') as file:
-            if project.path.endswith(".csproj"):
-                print(f"\nProject Name: {project.name} {project.path}")
-                open_path(file_to_sln, file_to_csproj, project)
     except FileNotFoundError:
         print(FileNotFoundError)
-    except AttributeError:
-       print(project.path)
-#######################################################################################################################
-for guid, project in all_projects.items():
-    file_to_csproj = f"C:\Workspace\SigarePlus\SIG-3884_f005094_Harmonisation\SigareCSharp\{project.path}"
-    try:
-        with open(file_to_csproj, 'r') as file:
-            file_content = file.read()
-            if file_to_csproj.endswith(".csproj"):
-                framework_version = re.search(pattern=r"<TargetFramework.*?>(.*?)</TargetFramework.*?>",
-                                              string=file_content,
-                                              flags=re.M | re.I).group(1)
-                project.framework = framework_version
 
-            if file_to_csproj.endswith(".vcxproj"):
-                framework_version = re.search(pattern=r"<TargetFrameworkVersion.*?>(.*?)<.*?/TargetFrameworkVersion>",
-                                              string=file_content,
-                                              flags=re.M | re.I).group(1)
-                project.framework = framework_version
-    except FileNotFoundError:
-        print("\nFileNotFoundError")
-        print(file_to_csproj)
-    except PermissionError:
-        print("\nPermissionError: [Errno 13] Permission denied")
-        print(file_to_csproj)
-    except AttributeError:
-        project.framework = ''
 ########################################################################################################################
 
+# Parse dictionnary using GUID as key
+for guid, project in all_projects.items():
+
+    # Select folder where .sln file is
+    path_to_sln = re.findall(pattern=r"(.*\\)",
+                             string=fileSLN,
+                             flags=re.RegexFlag.M)
+
+    # Create path to projetc .csproj or .vcxproj
+    file_to_csproj = f"{path_to_sln[0]}\\{project.path}"
+
+    if project.path.endswith(".csproj"):
+        try:
+            # Open path if it's .csproj
+            with open(file_to_csproj, 'r') as file:
+
+                # Call open_path() function
+                open_path(file_to_csproj, project)
+                file_content = file.read()
+
+                # For .vcxproj we only look for framework => no missed refs
+                if file_to_csproj.endswith(".vcxproj"):
+                    framework_version = re.search(pattern=r"<TargetFrameworkVersion.*?>(.*?)<.*?/TargetFrameworkVersion>",
+                                                  string=file_content,
+                                                  flags=re.M | re.I).group(1)
+                    # Add project framework to class
+                    project.framework = framework_version
+                else:
+                    framework_version = re.search(pattern=r"<TargetFramework.*?>(.*?)</TargetFramework.*?>",
+                                                    string=file_content,
+                                                    flags=re.M | re.I).group(1)
+                    # Add project framework to class
+                    project.framework = framework_version
+
+        except FileNotFoundError:
+            print(FileNotFoundError)
+            print(file_to_csproj)
+        except PermissionError:
+            print(PermissionError)
+            print(file_to_csproj)
+        except AttributeError:
+            project.framework = ''
+
+########################################################################################################################
+
+# Display of the All_Projects. txt file
 try:
     with open("All_Projects.txt", "w") as file:
         allProjects = 0
@@ -165,6 +198,7 @@ try:
         for guid, project in all_projects.items():
             if not project.isRoot() or not project.noChild():
                 allProjects += 1
+                # For .vcxproj projects
                 if project.path.endswith(".vcxproj"):
                     file.write(f"{project.name}: {project.guid} => Project ToolsVersion: {project.framework}\n")
                     file.write(f"   .vcxproj: {project.path}\n")
@@ -177,6 +211,7 @@ try:
                         file.write(f"   Mother {count}: {mother.name} => Framework: {mother.framework}\n")
                     count = 0
                     file.write(f"\n\n")
+                # For .csproj projects
                 if project.path.endswith(".csproj"):
                     file.write(f"{project.name}: {project.guid} => Framework: {project.framework}\n")
                     file.write(f"   .csproj : {project.path}\n")
@@ -193,15 +228,18 @@ try:
         file.close()
 
 except FileNotFoundError:
-    print("\nFileNotFoundError")
+    print(FileNotFoundError)
 except PermissionError:
-    print("\nPermissionError: [Errno 13] Permission denied")
+    print(PermissionError)
+
 ########################################################################################################################
 
+# Display of the Independent_Projects.txt file
 try:
     with open("Independent_Projects.txt", "w") as file:
         Independent_Projects = 0
         for guid, project in all_projects.items():
+            # Looking for projects that don't have mothers nor child in solution
             if project.isRoot() and project.noChild():
                 Independent_Projects += 1
                 file.write(f"{project.name} {project.guid} => Framework: {project.framework}\n")
@@ -210,19 +248,24 @@ try:
         file.close()
 
 except FileNotFoundError:
-    print("\nFileNotFoundError")
+    print(FileNotFoundError)
 except PermissionError:
-    print("\nPermissionError: [Errno 13] Permission denied")
+    print(PermissionError)
+
 ########################################################################################################################
 
-s = 0
+# Display of the Tree.txt file
+space = 0
 try:
     with open("Tree.txt", "w") as file:
         for guid, project in all_projects.items():
+            # Looking for a isRoot() project to build the tree from it
             if project.isRoot():
-                tree(file, project, s)
+                tree(file, project, space)
+        print("Build successful: \n=> Check for .txt files updates in this script's folder.")
 except FileNotFoundError:
-    print("\nFileNotFoundError")
+    print(FileNotFoundError)
 except PermissionError:
-    print("\nPermissionError: [Errno 13] Permission denied")
+    print(PermissionError)
+
 ########################################################################################################################
